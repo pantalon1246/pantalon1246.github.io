9338e546e8c7ad0b0e0ec6d665f74f9a24e45b4d
Pasha <example@gmail.com>
2022-09-30 14:43:23 +0300

    fs

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'pshpad@yandex.ru';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Password = 'qertjlpnjndvbsiz';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('pshpad@yandex.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('nifecet113@cebaike.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
